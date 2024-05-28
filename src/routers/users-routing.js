92cd4b3334e978f232ec595f0aeecb8079991191
balaraju404 <balarajugandham1@gmail.com>
2024-05-28 13:53:37 +0530

    changed image upload size to 10mb

--- src/routers/users-routing.js
+++ src/routers/users-routing.js
@@ -130,7 +130,7 @@ router.delete('/users/me', auth, async (req, res) => {
 
 const upload = multer({
     limits: {
-        fileSize: 1000000
+        fileSize: 10000000
     },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(jpg|jpeg|png)$/gm)) {

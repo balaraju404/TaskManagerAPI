28188ebe5ddb461e675e1da4e46f0fa3e4e72e92
Balaraju Gandham <balarajugandham1@gmail.com>
2025-02-21 20:28:55 +0530

    Update mongoose.js

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 // Load environment variables from .env file
 dotenv.config();
